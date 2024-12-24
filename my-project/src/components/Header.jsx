196fb0daeaf4202030a260818a65115b60036453
hadilbenyettou <hben7065@gmail.com>
2024-12-24 17:55:59 +0100

    .

---
+++ my-project/src/components/Header.jsx
@@ -0,0 +1,26 @@
+function Header() {
+  return (
+    <div className="flex flex-col md:flex-row justify-between items-center mt-8 max-w-[1400px] mx-auto">
+      {/* Text Section */}
+      <div className="flex flex-col justify-center items-start p-8 md:w-1/2">
+        <h1 className="text-green-800 text-8xl md:text-7xl font-bold leading-tight font-[PlayfairDisplay]">
+          Welcome to Fork & Spoon
+        </h1>
+        <p className="text-gray-800 text-xl md:text-2xl mt-4 font-light">
+          Experience the perfect blend of freshness, flavor, and elegance in every bite.
+        </p>
+      </div>
+
+      {/* The image */}
+      <div className="md:w-1/2">
+        <img 
+          src="/images/pngwing.com.png" 
+          alt="Background" 
+          className="w-full h-auto rounded-lg "
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- my-project/src/components/Main.jsx
+++ my-project/src/components/Main.jsx
@@ -1,16 +1,10 @@
+import Header from './Header';
 import NavBar from './NavBar';
 function Main() {
   return (
     <div>
       <NavBar />
-      <div className="flex justify-center mt-8">
-        {/* The image with full width and a max-width to make it large */}
-        <img 
-          src="/images/pic.avif" 
-          alt="Background" 
-          className="w-full max-w-[1400px] h-auto" 
-        />
-      </div>
+      <Header/>
     </div>
   );
 }
